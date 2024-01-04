69eae67eb616c569a4cf7d2db58ab32d44983de7
Lena Araújo <lenaaraujo92@gmail.com>
2024-01-04 13:35:40 -0300

    8 kyu - exclamation marks series 1

--- 8kyu/exclamationMarksSeries1.js
+++ 8kyu/exclamationMarksSeries1.js
@@ -13,9 +13,15 @@
 
 // My answer:
 
-const remove = (s) => s.replace(/!$/, "");
+function remove(s) {
+  return s.endsWith("!") ? s.slice(0, -1) : s;
+}
 
 // My reasoning:
+// 1. I have to check if the string ends with "!", so I use the very intuitively named method endsWith(), which returns a boolean.
+// 2. Now I use a conditional ternary operator to ask "Does s end with '!'?".
+// 3. If it does, then I trim it with slice. For the first parameter, I put the index 0, to start slicing at the beginning of the string; for the second, I choose the index -1 to count backwards and slice the last index of the string i.e. "!".
+// 4. If it doesn't, I return the original string.
 
 // Kata from https://www.codewars.com/kata/57fae964d80daa229d000126/javascript
 // 2024.01.02
