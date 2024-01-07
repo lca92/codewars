ecfba552f8a749e20192973e9e4f03e88772b1ec
Lena Araújo <lenaaraujo92@gmail.com>
2024-01-06 21:13:06 -0300

    8 kyu - training js 3

--- 8kyu/20240105_trainingJS3.js
+++ 8kyu/20240105_trainingJS3.js
@@ -16,7 +16,47 @@
 
 // My answer:
 
+var a1 = "A",
+  a2 = "a",
+  b1 = "B",
+  b2 = "b",
+  c1 = "C",
+  c2 = "c",
+  d1 = "D",
+  d2 = "d",
+  e1 = "E",
+  e2 = "e",
+  n1 = "N",
+  n2 = "n";
+function Dad() {
+  //select some variable to combine "Dad"
+  return d1 + a2 + d2;
+}
+function Bee() {
+  //select some variable to combine "Bee"
+  return b1 + e2 + e2;
+}
+function banana() {
+  //select some variable to combine "banana"
+  return b2 + a2 + n2 + a2 + n2 + a2;
+}
+
+//answer some questions if you finished works above
+function answer1() {
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer2() {
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer3() {
+  //the answer should be "yes" or "no"
+  return "yes";
+}
+
 // My reasoning:
+// 1. No thoughts, just vibes.
 
 // Kata from https://www.codewars.com/kata/571edea4b625edcb51000d8e
 // 2024.01.05
