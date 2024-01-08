f122f5e1643a2cd550e7223488288eb0e511870b
Lena Araújo <lenaaraujo92@gmail.com>
2024-01-08 10:48:30 -0300

    kyu 8 - a wolf in sheep\'s clothing

--- 8kyu/009_aWolfInSheepsClothing.js
+++ 8kyu/009_aWolfInSheepsClothing.js
@@ -1,3 +1,5 @@
+// #9
+
 // A wolf in sheep's clothing
 
 // Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
