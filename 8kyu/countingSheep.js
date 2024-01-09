32cb46a217514641034174d3a63e5afd6aebd37f
Lena Araújo <lenaaraujo92@gmail.com>
2024-01-08 23:29:26 -0300

    8 kyu - counting sheep

--- 8kyu/countingSheep.js
+++ 8kyu/countingSheep.js
@@ -21,6 +21,6 @@ const countSheeps = (arrayOfSheeps) => arrayOfSheeps.filter(Boolean).length;
 // My reasoning:
 // 1. Let's use filter() to leave out the missing sheep. If we use Boolean object as a parameter, it will only count the true values.
 // 2. filter() will return a shallow copy of the array with only the filtered values. If we use the length property, we can count how many sheep there are in this array.
-// 3. Bad values are already checked out when we use Boolean, because null and undefined turns into false.
+// 3. Bad values are already checked out when we use Boolean, because null and undefined turn into false.
 
 // Kata from https://www.codewars.com/kata/54edbc7200b811e956000556/javascript
