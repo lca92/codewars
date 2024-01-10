f2fc76a0e46cdcb26102abf034a8b97849c50b14
Lena Araújo <lenaaraujo92@gmail.com>
2024-01-10 20:54:42 -0300

    kyu 8 - 004

---
+++ 8kyu/004_5WithoutNumbers.js
@@ -0,0 +1,18 @@
+// 5 without numbers !!
+
+// DESCRIPTION:
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+// My answer:
+
+const unusualFive = () => "cinco".length;
+
+// My reasoning:
+// 1. I'll pass the tests if I write any five-character string and ask for its length.
+// 2. Why not write "five" itself, but at the same time show off my Portuguese knowledge?
+
+// Kata from https://www.codewars.com/kata/59441520102eaa25260000bf/javascript
