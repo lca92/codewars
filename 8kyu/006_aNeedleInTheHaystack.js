6823602d17f3b35cfacd3d45a04a9504284c8643
Lena Araújo <lenaaraujo92@gmail.com>
2024-01-11 21:38:28 -0300

    kyu 8 - 006

---
+++ 8kyu/006_aNeedleInTheHaystack.js
@@ -0,0 +1,24 @@
+// A Needle in the Haystack
+
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// Example(Input --> Output)
+
+// ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5"
+// Note: In COBOL, it should return "found the needle at position 6"
+
+// My answer:
+
+const findNeedle = (haystack) =>
+  `found the needle at position ${haystack.indexOf("needle")}`;
+
+// My reasoning:
+// 1. Pretty straightforward. indexOf() returns the index of an element.
+
+// Kata from https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/javascript
