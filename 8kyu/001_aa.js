0e873e8d99ef6aeb3f307a3d0df37c74ce349282
Lena Araújo <lenaaraujo92@gmail.com>
2024-01-17 06:55:49 -0300

    kyu 8 - 001

--- 8kyu/001_aa.js
+++ 8kyu/001_aa.js
@@ -16,6 +16,6 @@ const a = [];
 // 2. Anyways. It's about truthy and falsy values and the difference between abstract equality (==) and strict equality (===).
 // 3. a == false: In JavaScript, when you use the abstract equality operator (==), it performs type coercion. An empty array [] is truthy, so a == false will be false.
 // 4. !a == false: The logical NOT (!) operator negates the truthiness of a value. Since an empty array is truthy, !a will be false, and false == false is true.
-// a == !a: Here, you're comparing the empty array a to its negation !a. !a is false, so this expression is equivalent to a == false, which is false.
+// 5. a == !a: Here, you're comparing the empty array a to its negation !a. !a is false, so this expression is equivalent to a == false, which is false.
 
 // Kata from https://www.codewars.com/kata/59f9796cffe75f9299000025/javascript
