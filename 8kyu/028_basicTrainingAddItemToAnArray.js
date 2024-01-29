23b64b0d29cfae381eafa376c8ea527efae8a66c
Lena Araújo <lenaaraujo92@gmail.com>
2024-01-29 15:11:32 -0300

    kyu 8 - 028

---
+++ 8kyu/028_basicTrainingAddItemToAnArray.js
@@ -0,0 +1,17 @@
+// Basic Training: Add item to an Array
+
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// var websites = [];
+
+// My answer:
+
+websites.push("codewars");
+
+// My reasoning:
+// 1. The push() method of Array instances adds the specified elements to the end of an array and returns the new length of the array.
+
+// Kata from https://www.codewars.com/kata/511f0fe64ae8683297000001/javascript
