e01b9cd307c7dfeaee93571ddb566262b55e4cff
Lena Araújo <lenaaraujo92@gmail.com>
2024-02-03 08:05:38 -0300

    kyu 8 - 051

---
+++ 8kyu/051_chuckNorrisVIITrueOrFalseBeginner.js
@@ -0,0 +1,20 @@
+// Chuck Norris VII - True or False? (Beginner)
+
+// It's a well known fact that anything Chuck Norris wants, he gets. As a result Chuck very rarely has to use the word false.
+
+// It is a less well known fact that if something is true, and Chuck doesn't want it to be, Chuck can scare the truth with his massive biceps, and it automatically becomes false.
+
+// Your task is to be more like Chuck (ha! good luck!). You must return false without ever actually using the word false...
+
+// Go show some truth who's boss!
+
+// My answer:
+
+const ifChuckSaysSo = () => !true;
+
+// My reasoning:
+// 1. We have to return a falsy value.
+// 2. The not operator (!) evaluates an element to its logical opposite.
+// 3. The opposite of true is false.
+
+// Kata from https://www.codewars.com/kata/570669d8cb7293a2d1001473/javascript
