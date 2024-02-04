3a6836e467b349ba4f972f09a4e698c0a7597ef6
Lena Araújo <lenaaraujo92@gmail.com>
2024-02-04 15:46:25 -0300

    kyu 8 - 063

---
+++ 8kyu/063_convertABooleanToAString.js
@@ -0,0 +1,14 @@
+// Convert a Boolean to a String
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// My answer:
+
+const booleanToString = (b) => b.toString();
+
+// My reasoning:
+// 1. The toString() method of Boolean values returns a string representing the specified boolean value.
+
+// Kata from https://www.codewars.com/kata/551b4501ac0447318f0009cd/javascript
