f53db8f13499ec6cd171dbddb758f1d93382cad4
Lena Araújo <lenaaraujo92@gmail.com>
2024-02-05 19:15:36 -0300

    kyu 8 - 067

---
+++ 8kyu/067_convertBooleanValuesToStringsYesOrNo.js
@@ -0,0 +1,15 @@
+// Convert boolean values to strings 'Yes' or 'No'.
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// My answer:
+
+const boolToWord = (bool) => (bool ? `Yes` : `No`);
+
+// My reasoning:
+// 1. If an element exists, it is already true.
+// 2. So if we ask: "Does bool exist?"...
+// 3. ... We return 'Yes' if it does...
+// 4. ... And 'No' if it doesn't.
+
+// Kata from https://www.codewars.com/kata/53369039d7ab3ac506000467/javascript
