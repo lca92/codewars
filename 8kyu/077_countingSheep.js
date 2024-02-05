7b2152143398ba1d11208be20c315f4ed239cea4
Lena Araújo <lenaaraujo92@gmail.com>
2024-02-04 23:14:33 -0300

    kyu 8 - 077

---
+++ 8kyu/077_countingSheep.js
@@ -0,0 +1,26 @@
+// Counting sheep...
+
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+// My answer:
+
+const countSheeps = (arrayOfSheeps) => arrayOfSheeps.filter(Boolean).length;
+
+// My reasoning:
+// 1. Let's use filter() to leave out the missing sheep. If we use Boolean object as a parameter, it will only count the true values.
+// 2. filter() will return a shallow copy of the array with only the filtered values. If we use the length property, we can count how many sheep there are in this array.
+// 3. Bad values are already checked out when we use Boolean, because null and undefined turn into false.
+
+// Kata from https://www.codewars.com/kata/54edbc7200b811e956000556/javascript
