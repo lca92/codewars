18c3b568573ca4450a4ae9c83233a23e6cccbd11
Lena Araújo <lenaaraujo92@gmail.com>
2024-02-11 12:27:34 -0300

    kyu 8 - 141

---
+++ 8kyu/141_fundamentalsReturn.js
@@ -0,0 +1,38 @@
+// Fundamentals: Return
+
+// Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+// Please use the following function names:
+
+// addition = add
+
+// multiply = multiply
+
+// division = divide (both integer and float divisions are accepted)
+
+// modulus = mod
+
+// exponential = exponent
+
+// subtraction = subt
+
+// Note: All math operations will be: a (operation) b
+
+// My answer:
+
+const add = (a, b) => a + b;
+
+const divide = (a, b) => a / b;
+
+const multiply = (a, b) => a * b;
+
+const mod = (a, b) => a % b;
+
+const exponent = (a, b) => a ** b;
+
+const subt = (a, b) => a - b;
+
+// My reasoning:
+// 1. For each function, we have to write the equivalent return.
+
+// Kata from https://www.codewars.com/kata/55a5befdf16499bffb00007b/javascript
