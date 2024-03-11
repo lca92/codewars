357e1971f295234539d4428dcd2283fdaf0b6c9e
Lena Araújo <lenaaraujo92@gmail.com>
2024-03-10 21:11:05 -0300

    kyu 8 - 002

--- 8kyu/002_thisIsAProblem.js
+++ 8kyu/002_thisIsAProblem.js
@@ -17,6 +17,16 @@
 
 // My answer:
 
+function NameMe(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  this.name = first + " " + last;
+}
+
 // My reasoning:
+// 1. We have to debug the function. We have to change the third line of the function's body.
+// 2. In this case, "this" is used to access the object that the function is used on.
+// 3. To access the name property, we use "this.name".
+// 4. Then, we specify that name is the first name and the last name combined, with a space between.
 
 // Kata from https://www.codewars.com/kata/547c71fdc5b2b38db1000098/javascript
