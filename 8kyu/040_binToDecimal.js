9a1c15c85ca326c208dcc2bc46ffadc2933a5618
Lena Araújo <lenaaraujo92@gmail.com>
2024-03-23 09:45:16 -0300

    kyu 8 - 040

---
+++ 8kyu/040_binToDecimal.js
@@ -0,0 +1,9 @@
+// Bin to Decimal
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// My answer:
+
+// My reasoning:
+
+// Kata from https://www.codewars.com/kata/57a5c31ce298a7e6b7000334
