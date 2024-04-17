1e84d08b966a9fb1b0d226b9bd870e041c166d21
Lena Araújo <lenaaraujo92@gmail.com>
2024-04-17 11:14:10 -0300

    done

---
+++ 8kyu/basicVariableAssignment.js
@@ -0,0 +1,15 @@
+// Basic variable assignment
+
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+// My answer:
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+
+// My reasoning:
+// 1. The original code had equality operators (==) instead of the equal sign we should use when declaring a variable.
+// 2. Equality operators should be used when we want to compare two operands.
+
+// Kata from https://www.codewars.com/kata/50ee6b0bdeab583673000025/javascript
---
+++ 8kyu/exclamationMarksSeries6RemoveNExclamationMarksInTheSentenceFromLeftToRight.js
@@ -0,0 +1,1 @@
+// Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right
