83895b64e7b6920295ba4c733e25cff9365e9c3b
Lena Araújo <lenaaraujo92@gmail.com>
2024-04-25 13:00:23 -0300

    delete

--- 8kyu/basicVariableAssingment.js
+++
@@ -1,9 +0,0 @@
-// Basic variable assignment
-
-// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
-
-// My answer:
-
-// My reasoning:
-
-// Kata from https://www.codewars.com/kata/50ee6b0bdeab583673000025
