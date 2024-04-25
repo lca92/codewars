163dc418fcb1bc0e8d4d474a94e22338fb2de8fe
Lena Araújo <lenaaraujo92@gmail.com>
2024-04-25 20:17:17 -0300

    done

--- 8kyu/centuryFromYear.js
+++ 8kyu/centuryFromYear.js
@@ -18,5 +18,10 @@
 const century = (year) => Math.ceil(year / 100);
 
 // My reasoning:
+// 1. Math.ceil() rounds up and returns the smallest integer greater than or equal to a given number.
+// 2. If we divide year by a hundred, we'll have the correct century for years ending in "00".
+// 3. For every other case, we're left with a decimal number.
+// 4. If we round our calculations up, we get the correct century.
+// 5. And rounding up doesn't affect an integer.
 
 // Kata from https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/javascript
