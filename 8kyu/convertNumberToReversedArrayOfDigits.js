5e21dc7e687570699a99f73c93c58f0823d3861b
Lena Araújo <lenaaraujo92@gmail.com>
2024-05-07 11:27:54 -0300

    missing explanation

---
+++ 8kyu/convertNumberToReversedArrayOfDigits.js
@@ -0,0 +1,21 @@
+// Convert number to reversed array of digits
+
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example(Input => Output):
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+
+// My answer:
+
+const digitize = (n) =>
+  n
+    .toString()
+    .split("")
+    .reverse()
+    .map((string) => Number(string));
+
+// My reasoning:
+
+// Kata from https://www.codewars.com/kata/5583090cbe83f4fd8c000051/javascript
