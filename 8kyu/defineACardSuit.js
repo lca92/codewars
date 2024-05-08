1823989a2dc703a84fc6e3405a8ec5f3242e416a
Lena Araújo <lenaaraujo92@gmail.com>
2024-05-08 11:10:56 -0300

    missing explanation

---
+++ 8kyu/defineACardSuit.js
@@ -0,0 +1,22 @@
+// Define a card suit
+
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// Our deck (is preloaded):
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+// My answer:
+const defineSuit = (card) => {
+  if (card.includes("♣")) return "clubs";
+  if (card.includes("♦")) return "diamonds";
+  if (card.includes("♥")) return "hearts";
+  if (card.includes("♠")) return "spades";
+};
+
+// My reasoning:
+
+// Kata from https://www.codewars.com/kata/5a360620f28b82a711000047/javascript
