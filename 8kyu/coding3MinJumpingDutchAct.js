e2834bb54b31790da84b6f752cf0e3d188e9c701
Lena Araújo <lenaaraujo92@gmail.com>
2024-05-09 10:23:32 -0300

    done

--- 8kyu/coding3MinJumpingDutchAct.js
+++ 8kyu/coding3MinJumpingDutchAct.js
@@ -41,5 +41,12 @@ const sc = (floor) => {
 };
 
 // My reasoning:
+// 1. If they are already on the ground, we return an empty string ("").
+// 2. For each floor Mr. Despair passes during his fall, he will scream "Aa~ ". For that, we use the repeat() method.
+// 3. repeat() takes in a number n and repeats a string n times. In our case, we use floor - 1, because the last one is going to have a different scream.
+// 4. When Mr. Despair hits the ground, he screams "Pa!"
+// 5. Now we use a ternary operator to ask ourselves: "Is the height less than or equal to 6 floors?"
+// 6. If it is, we append a final scream.
+// 7. If it isn't, Mr. Despair dies on impact and we return an empty string.
 
 // Kata from https://www.codewars.com/kata/570bcd9715944a2c8e000009/javascript
