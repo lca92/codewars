58fc8e51ffe6ded149493b29d4b3726b0c19a7f2
Lena Araújo <lenaaraujo92@gmail.com>
2024-05-10 21:22:45 -0300

    done

--- 8kyu/compareWithinMargin.js
+++ 8kyu/compareWithinMargin.js
@@ -34,6 +34,12 @@ const closeCompare = (a, b, margin = 0) =>
   margin >= Math.abs(a - b) ? 0 : a < b ? -1 : 1;
 
 // My reasoning:
-// 1. 
+// 1. Math.abs() returns the absolute value of a number.
+// 2. To evaluate the distance between two points we need to subtract one from the other (with absolute values).
+// 3. Now we use the ternary operator to ask ourselves: "Is the margin greater than or equal to the distance?".
+// 4. If it is, we return 0.
+// 5. If it isn't, we ask ourselves: "Is a less than b?".
+// 6. If it is, we return -1.
+// 7. If it isn't, we return 1.
 
 // Kata from https://www.codewars.com/kata/56453a12fcee9a6c4700009c/javascript
