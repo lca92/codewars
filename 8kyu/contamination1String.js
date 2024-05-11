7c5c1eae468ebf7759c1ed28ab40edac4492c1fd
Lena Araújo <lenaaraujo92@gmail.com>
2024-05-11 12:37:25 -0300

    done

--- 8kyu/contamination1String.js
+++ 8kyu/contamination1String.js
@@ -19,5 +19,8 @@
 const contamination = (text, char) => char.repeat(text.length);
 
 // My reasoning:
+// 1. We don't actually change the string from the input.
+// 2. We use repeat() on the char string to return another string that repeats the first one a certain amount of times.
+// 3. We use the length property to check how many characters there are in the text string and pass this value to repeat().
 
 // Kata from https://www.codewars.com/kata/596fba44963025c878000039/javascript
