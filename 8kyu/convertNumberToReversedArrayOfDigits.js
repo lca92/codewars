d37358710ec97205c9ca47a924ad807428c1d3f3
Lena Araújo <lenaaraujo92@gmail.com>
2024-05-12 15:46:54 -0300

    done

--- 8kyu/convertNumberToReversedArrayOfDigits.js
+++ 8kyu/convertNumberToReversedArrayOfDigits.js
@@ -17,5 +17,9 @@ const digitize = (n) =>
     .map((string) => Number(string));
 
 // My reasoning:
+// 1. toString() transforms a number into a string. We do this to be able to use the split() method of String.
+// 2. split() takes a pattern and divides this string into an ordered list of substrings by searching for the pattern (in our case, an empty string).
+// 3. Now we have an array with strings containing each individual number. We use reverse() to reverse the order of the elements.
+// 4. Still, we need to change the type of the elements from String to Number. We can use map() to make this change for each string.
 
 // Kata from https://www.codewars.com/kata/5583090cbe83f4fd8c000051/javascript
