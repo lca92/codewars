b0dfa71e7255583368f53669b5cc7fc0ea54e963
Lena Araújo <lenaaraujo92@gmail.com>
2024-05-20 11:01:26 -0300

    done

---
+++ 8kyu/function2SquaringAnArgument.js
@@ -0,0 +1,12 @@
+// Function 2 - squaring an argument
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// My answer:
+
+const square = (number) => number ** 2;
+
+// My reasoning:
+// 1. The exponentiation (**) operator returns the result of raising the first operand to the power of the second operand.
+
+// Kata from https://www.codewars.com/kata/523b623152af8a30c6000027/javascript
