dfdab91cb3b0926b2f21bc80baba6c2b4c994a95
Lena Araújo <lenaaraujo92@gmail.com>
2024-05-20 10:55:40 -0300

    done

---
+++ 8kyu/function1HelloWorld.js
@@ -0,0 +1,15 @@
+// Function 1 - hello world
+
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+// My answer:
+
+const greet = () => `hello world!`;
+
+// My reasoning:
+// 1. We just have to return the string "hello world!"
+
+// Kata from https://www.codewars.com/kata/523b4ff7adca849afe000035/javascript
