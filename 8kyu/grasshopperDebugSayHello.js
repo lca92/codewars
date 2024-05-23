a69fe4df5f5a9967bd3048bcf64cc3c28b5f579e
Lena Araújo <lenaaraujo92@gmail.com>
2024-05-23 15:46:15 -0300

    missing explanation

---
+++ 8kyu/grasshopperDebugSayHello.js
@@ -0,0 +1,16 @@
+// Grasshopper - Debug sayHello
+
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+// My answer:
+
+const sayHello = (name) => `Hello, ${name}`;
+
+// My reasoning:
+
+// Kata https://www.codewars.com/kata/5625618b1fe21ab49f00001f/javascript
